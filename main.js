5704a6e3fe9d8d50a08f599ab1a633ce3dfaf33f
Antti Keränen <detegr@gmail.com>
2013-10-10 19:48:39 +0300

    Modularized some stuff

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+"use strict";
+
 tdl.require('tdl.buffers');
 tdl.require('tdl.fast');
 tdl.require('tdl.math');
@@ -6,53 +8,125 @@ tdl.require('tdl.primitives');
 tdl.require('tdl.programs');
 tdl.require('tdl.webgl');
 
-window.onload=initialize;
-
-function initialize()
+window.onload=function()
 {
-    canvas = document.getElementById("canvas");
-    gl = tdl.webgl.setupWebGL(canvas);
-    if(!gl)
-        return;
+	window.canvas = document.getElementById("canvas");
+	window.gl = tdl.webgl.setupWebGL(canvas);
+	window.framecount=0;
 
-    var program = tdl.programs.loadProgramFromScriptTags("shader/mvp", "shader/plain-white");
-    var arrays = tdl.primitives.createSphere(0.2, 10, 100);
-    model = new tdl.models.Model(program, arrays);
+	function ProgramManager() {}
+	ProgramManager.programs={};
+	ProgramManager.LoadProgram=function(vec,frag)
+	{
+		var catenated=vec+frag;
+		var progs=ProgramManager.programs;
+		if(!progs[catenated])
+			progs[catenated]=tdl.programs.loadProgramFromScriptTags(vec, frag);
+		return progs[catenated];
+	};
 
-    sharedUniforms = {
-        P: new Float32Array(16)
-    };
-    perObjectUniforms = {
-        MV: new Float32Array(16)
-    };
+	function ModelManager() {}
+	ModelManager.models={};
+	ModelManager.GetModel=function(drawableobject)
+	{
+		var models=ModelManager.models;
+		var classname=drawableobject.constructor.name;
+		if(!models[classname])
+			models[classname]=new tdl.models.Model(drawableobject.program, drawableobject.shape);
+		return models[classname];
+	};
 
-    view = new Float32Array(16);
-    modelmatrix = new Float32Array(16);
+	function DrawableObject(mvp, shader) {
+		this.uniforms={
+			MV: new Float32Array(16)
+		};
+		this.shape=null;
+		this.modelMatrix = new Float32Array(16);
+		this.rotationMatrix = new Float32Array(16);
+		this.program = ProgramManager.LoadProgram(mvp, shader);
+		this.Translate(0,0,0);
+		this.Rotate(0,0,0);
+		return this;
+	}
+	DrawableObject.prototype={
+		constructor: DrawableObject,
+		Draw: function()
+		{
+			tdl.fast.matrix4.mul(this.uniforms.MV, this.modelMatrix, this.rotationMatrix);
+			this.model.draw(this.uniforms);
+		},
+		Translate: function(x,y,z)
+		{
+			tdl.fast.matrix4.translation(this.modelMatrix, [x,y,z]);
+		},
+		Rotate: function(x,y,z)
+		{
+			tdl.fast.matrix4.rotationX(this.rotationMatrix, x);
+			tdl.fast.matrix4.rotationY(this.rotationMatrix, y);
+			tdl.fast.matrix4.rotationZ(this.rotationMatrix, z);
+		}
+	};
 
-    //tdl.fast.matrix4.perspective(sharedUniforms.P, tdl.math.degToRad(60), canvas.clientWidth / canvas.clientHeight, 1, 5000);
-    var aspectratio = canvas.clientWidth / canvas.clientHeight;
-    tdl.fast.matrix4.ortho(sharedUniforms.P, -1, 1, -1.0/aspectratio, 1.0/aspectratio, 1, 5000);
+	function Sphere()
+	{
+		Sphere.prototype.constructor=Sphere;
+		DrawableObject.apply(this, ["shader/mvp", "shader/plain-white"]);
+		this.shape=tdl.primitives.createSphere(0.1, 10, 100);
+		this.model=ModelManager.GetModel(this);
+		return this;
+	}
+	Sphere.prototype=DrawableObject.prototype;
 
-    framecount=0;
-    draw();
-}
+	function Screen()
+	{
+		this.sharedUniforms = {
+			P: new Float32Array(16)
+		};
+		this.objects={};
+		this.aspectRatio = canvas.clientWidth / canvas.clientHeight;
+		tdl.fast.matrix4.ortho(this.sharedUniforms.P, -1, 1, -1.0/this.aspectRatio, 1.0/this.aspectRatio, 1, 5000);
+		return this;
+	}
+	Screen.prototype={
+		Draw: function()
+		{
+			gl.colorMask(true, true, true, true);
+			gl.clear(gl.COLOR_BUFFER_BIT);
+			for(var cls in this.objects)
+			{
+				ModelManager.models[cls].drawPrep(this.sharedUniforms);
+				for(var i=0, len=this.objects[cls].length; i<len; ++i)
+				{
+					this.objects[cls][i].Draw();
+				}
+			}
+		},
+		AddObject: function(o)
+		{
+			if(!this.objects[o.constructor.name])
+				this.objects[o.constructor.name]=[o];
+			else this.objects[o.constructor.name].push(o);
+		}
+	};
 
-function draw()
-{
-    ++framecount;
-    tdl.webgl.requestAnimationFrame(draw, canvas);
-    var rotationmatrix=new Float32Array(16);
-    tdl.fast.matrix4.rotationZ(rotationmatrix, framecount/100);
-
-    gl.colorMask(true, true, true, true);
-    gl.clear(gl.COLOR_BUFFER_BIT);
-
-    model.drawPrep(sharedUniforms);
-
-    for(var i = 0; i < 3; ++i)
-    {
-        tdl.fast.matrix4.translation(modelmatrix, [i/2, 0, 0]);
-        tdl.fast.matrix4.mul(perObjectUniforms.MV, modelmatrix, rotationmatrix);
-        model.draw(perObjectUniforms);
-    }
-}
+	(function initialize()
+	{
+		if(!gl) return;
+		var scrn=new Screen();
+		var s=new Sphere;
+		s.Translate(0.2, 0, 0);
+		var s2=new Sphere;
+		s2.Translate(0.4, 0, 0);
+		scrn.AddObject(s);
+		scrn.AddObject(s2);
+		scrn.AddObject(new Sphere);
+		(function draw()
+		{
+			++framecount;
+			tdl.webgl.requestAnimationFrame(draw, canvas);
+			s.Rotate(0,0,framecount/50);
+			s2.Rotate(0,0,framecount/100);
+			scrn.Draw();
+		})();
+	})();
+};
